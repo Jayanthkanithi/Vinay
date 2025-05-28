f96d1a2f9eae847cd706816619147d552aca1847
Jayanthkanithi <jayanthkanithi2003@gmail.com>
2025-05-28 10:10:06 +0530

    education details update

--- src/app/components/Education/Education.tsx
+++ src/app/components/Education/Education.tsx
@@ -6,8 +6,18 @@ const Education = () => {
         <h2>Education</h2>
         <div className="education__item">
           <h3> B.Tech - Artificial Intelligence And Data Science</h3>
-          <p> KL UNIVERSITY, 2021 – 2025</p>
+          <p> KL UNIVERSITY, Vijayawada, 2021 – 2025</p>
           <p>CGPA: 8.98 / 10</p>
+        </div>
+        <div className="education__item">
+          <h3>  12th standard</h3>
+          <p> TIRUMALA EDUCATIONAL INSTITUTIONS, Bhimavaram, 2019 – 2021</p>
+          <p>Percentage: 92.00</p>
+        </div>
+        <div className="education__item">
+          <h3>10th standard</h3>
+          <p>SRI CHAITANYA TECHNO SCHOOL, Tadepalliguden, 2019</p>
+          <p>GPA: 9.5 / 10</p>
         </div>
       </div>
     </section>
